31512241a2e88193169cf7fb9bab548779b15333
Rinasham <rinashamau@gmail.com>
2022-07-27 05:37:29 +1000

    [add] modal nav base

---
+++ client/src/components/organisms/navModal/NavModal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./NavModal.module.css";
+
+export default function NavModal() {
+  return (
+    <div className={styles.wrapper}>
+      <p>menu</p>
+      <p>aaa</p>
+    </div>
+  );
+}
