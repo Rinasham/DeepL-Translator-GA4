27f8b93889bbc86ce732ae4689beb9ddfbdf392f
Rinasham <rinashamau@gmail.com>
2022-07-27 05:37:06 +1000

    [add] open modal button in LP

---
+++ client/src/components/molecules/title/modalButton/ModalButton.tsx
@@ -0,0 +1,11 @@
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
+import { COLORS } from "../../../colors";
+import styles from "./ModalButton.module.css";
+
+export default function ModalButton() {
+  return (
+    <div className={styles.buttonWrapper}>
+      <OpenInFullIcon style={{ fontSize: "40px" }} />
+    </div>
+  );
+}
