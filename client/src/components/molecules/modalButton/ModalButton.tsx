1df3cfb79f903218d569853c578bb4b04d753a5c
Rinasham <rinashamau@gmail.com>
2022-07-27 06:29:25 +1000

    [fix] modal open button => position: fixed

--- client/src/components/molecules/modalButton/ModalButton.tsx
+++ client/src/components/molecules/modalButton/ModalButton.tsx
@@ -1,5 +1,5 @@
 import OpenInFullIcon from "@mui/icons-material/OpenInFull";
-import { COLORS } from "../../../colors";
+import { COLORS } from "../../colors";
 import styles from "./ModalButton.module.css";
 
 export default function ModalButton() {
