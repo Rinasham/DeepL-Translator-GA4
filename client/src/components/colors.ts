3e76f872cb3886a1e1c229770fe4124a25c546da
Rinasham <rinashamau@gmail.com>
2022-07-27 09:39:34 +1000

    [fix] css

--- client/src/components/colors.ts
+++
@@ -1,9 +0,0 @@
-export const COLORS = {
-  ASH_BLACK: "#384449",
-  ASH_BEIGH: "#d1bfb2",
-  OFF_WHITE: "#fdfbf6",
-  LIGHT_BLUE: "#5eb5c0",
-  DARK_BLUE: "#3d94aa",
-  DARK_GREEN: "#2e4448",
-  DARK_RED: "#931424",
-};
---
+++ client/src/components/constants/colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  ASH_BLACK: "#384449",
+  ASH_BEIGH: "#d1bfb2",
+  OFF_WHITE: "#fdfbf6",
+  LIGHT_BLUE: "#5eb5c0",
+  DARK_BLUE: "#3d94aa",
+  DARK_GREEN: "#2e4448",
+  DARK_RED: "#931424",
+};
+
+// /* 太字タイトル */
+// font-family: "Krona One", sans-serif;
+// font-family: "Italiana", serif;
+// font-family: "Khula", sans-serif;
+// font-family: "Special Elite", cursive;
+// font-family: 'Glass Antiqua', cursive;
