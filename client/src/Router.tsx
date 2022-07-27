8642ffe3782f2b643b04105a8b74b769c3a066b2
Rinasham <rinashamau@gmail.com>
2022-07-27 21:08:15 +1000

    [fix] add routers

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NotFound } from "./components/pages/errorPages/NotFound";
+import GameMain from "./components/pages/game/GameMain";
+import GameStart from "./components/pages/game/GameStart";
 import Main from "./components/pages/mainPage/Main";
+import Login from "./components/pages/registration/Login";
+import Signup from "./components/pages/registration/Signup";
 
 export const Router: () => JSX.Element = () => {
   return (
@@ -7,7 +12,11 @@ export const Router: () => JSX.Element = () => {
       <BrowserRouter>
         <Routes>
           <Route path={`/`} element={<Main />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path={`signup`} element={<Signup />} />
+          <Route path={`login`} element={<Login />} />
+          <Route path={`start`} element={<GameStart />} />
+          <Route path={`game`} element={<GameMain />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
