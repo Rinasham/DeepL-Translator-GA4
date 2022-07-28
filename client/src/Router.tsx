602164a131c186114edc25dbb4ade0dfa21654ba
Rinasham <rinashamau@gmail.com>
2022-07-28 19:25:41 +1000

    [add] some routes

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Account from "./components/pages/account/Account";
 import { NotFound } from "./components/pages/errorPages/NotFound";
-import GameMain from "./components/pages/game/GameMain";
-import GameStart from "./components/pages/game/GameStart";
 import Loading from "./components/pages/loading/Loading";
 import Main from "./components/pages/mainPage/Main";
 import Login from "./components/pages/registration/Login";
-import { Signup } from "./components/pages/registration/Signup";
+import { Authentication } from "./components/pages/registration/Authentication";
+import TranslatorStart from "./components/pages/translator/TranslatorStart";
+import TranslatorMain from "./components/pages/translator/TranslatorMain";
 
 export const Router: () => JSX.Element = () => {
   return (
@@ -15,10 +15,10 @@ export const Router: () => JSX.Element = () => {
         <Routes>
           <Route path={`/`} element={<Loading />} />
           <Route path={`/home`} element={<Main />} />
-          <Route path={`signup`} element={<Signup />} />
-          <Route path={`login`} element={<Login />} />
-          <Route path={`start`} element={<GameStart />} />
-          <Route path={`game`} element={<GameMain />} />
+          <Route path={`authentication`} element={<Authentication />} />
+          {/* <Route path={`login`} element={<Login />} /> */}
+          <Route path={`translator`} element={<TranslatorStart />} />
+          <Route path={`translator-main`} element={<TranslatorMain />} />
           <Route path={`account`} element={<Account />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
