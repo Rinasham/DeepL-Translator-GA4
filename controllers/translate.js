3c484ea37bfc1a0a31d78b11ea8f8f402bb5af8b
Rinasham <rinashamau@gmail.com>
2022-07-31 04:42:59 +1000

    [add] api transaction at BE

--- controllers/translate.js
+++ controllers/translate.js
@@ -38,26 +38,34 @@ router.post("/checkanswer/:language", (req, res) => {
   console.log(req.params.language);
   console.log(req.body);
 
-  let text_ja = ""; //TODO ここに出力する日本語を格納する => apiで送信
-  let text_en = "";
+  // let text_ja = ""; //TODO ここに出力する日本語を格納する => apiで送信
+  // let text_en = "";
+  let text = "";
+  let target_lang = ""; // 'JP' or 'EN'
 
-  // translate({
-  //   free_api: true, // ← フリープランのみ記載
-  //   text: "こんにちは、元気ですか？",
-  //   target_lang: "EN",
-  //   auth_key: process.env.DEEPL_API_KEY, // ここにDeeplのAPIキーを入力
-  // })
-  //   .then((result) => {
-  //     console.log(result.data);
-  //     const text_en = result.data.translations[0].text; //Deeplで翻訳された文章をtext_en変数に格納
-  //     console.log(text_en);
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
+  // whan the answer sent is JAPANESE
+  if (req.params.language === "ja") {
+    target_lang = "EN";
+  } else if (req.params.language === "en") {
+    target_lang = "JA";
+  }
 
-  res.json("ok");
+  translate({
+    free_api: true, // ← フリープランのみ記載
+    text: req.body.answer,
+    target_lang: target_lang,
+    auth_key: process.env.DEEPL_API_KEY, // ここにDeeplのAPIキーを入力
+  })
+    .then((result) => {
+      console.log(result.data);
+      const translated_text = result.data.translations[0].text; //Deeplで翻訳された文章をtext_en変数に格納
+      console.log(translated_text);
+      res.json({ translated_text: translated_text });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ success: false });
+    });
 });
-
 
 module.exports = router;
