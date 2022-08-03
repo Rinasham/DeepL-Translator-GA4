eea2027c656101e807a2b1177fda08fc6edd5e86
Rinasham <rinashamau@gmail.com>
2022-08-03 22:51:57 +1000

    [add] page top function

--- client/src/components/molecules/pageTop/PageTop.tsx
+++ client/src/components/molecules/pageTop/PageTop.tsx
@@ -3,8 +3,15 @@ import styles from "./PageTop.module.css";
 import KeyboardDoubleArrowUpSharpIcon from "@mui/icons-material/KeyboardDoubleArrowUpSharp";
 
 export default function PageTop() {
+  const onClickPageTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className={styles.pageTop}>
+    <div className={styles.pageTop} onClick={onClickPageTop}>
       <KeyboardDoubleArrowUpSharpIcon />
       <p className={styles.pageTopText}>PAGE TOP</p>
     </div>
